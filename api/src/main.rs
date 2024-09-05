6c043cfc1ad0a0d85e371a1628509d2ecc133cce
isk <0xisk@proton.me>
2024-09-05 23:31:00 +0200

    fix: ignore downlaod circuit files

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,20 +1,26 @@
-use std::sync::Mutex;
+use std::{sync::Mutex};
 
-use actix_web::{get, guard, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, guard, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 struct AppState {
-    app_name: String
+    app_name: String,
 }
 
 struct AppStateWithCounter {
-    counter: Mutex<i32>
+    counter: Mutex<i32>,
 }
 
-async fn index(data: web::Data<AppStateWithCounter>) -> impl Responder {
-    let mut counter = data.counter.lock().unwrap();
-    *counter += 1;
+// async fn index(data: web::Data<AppStateWithCounter>) -> impl Responder {
+//     let mut counter = data.counter.lock().unwrap();
+//     *counter += 1;
 
-    format!("Request number: {counter}")
+//     format!("Request number: {counter}")
+// }
+
+#[get("/")]
+async fn index(_req: HttpRequest) -> impl Responder {
+    "Welcome!"
 }
 
 #[post("/echo")]
@@ -55,23 +61,37 @@ async fn manual_hello() -> impl Responder {
 //     // .await
 // }
 
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .service(
+//                 web::scope("/")
+//                     .guard(guard::Host("www.rust-lang.org"))
+//                     .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
+//             )
+//             .service(
+//                 web::scope("/")
+//                     .guard(guard::Host("users.rust-lang.org"))
+//                     .route("", web::to(|| async { HttpResponse::Ok().body("user") })),
+//             )
+//             .route("/", web::to(HttpResponse::Ok))
+//     })
+//     .bind(("127.0.0.1", 8080))?
+//     .run()
+//     .await
+// }
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(
-                web::scope("/")
-                    .guard(guard::Host("www.rust-lang.org"))
-                    .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
-            )
-            .service(
-                web::scope("/")
-                    .guard(guard::Host("users.rust-lang.org"))
-                    .route("", web::to(|| async { HttpResponse::Ok().body("user") })),
-            )
-            .route("/", web::to(HttpResponse::Ok))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("./key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
+
+    HttpServer::new(|| App::new().service(index))
+        .bind_openssl("127.0.0.2:8080", builder)?
+        .run()
+        .await
 }
